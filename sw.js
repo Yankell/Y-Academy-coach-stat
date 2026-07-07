0969251dbe8c344e433c2858601e3ebe47638a95
Yankell <ykleps@gmail.com>
2026-07-07 11:52:17 +0200

    Ajout du mode hors connexion

--- sw.js
+++ sw.js
@@ -1,23 +1,20 @@
-const CACHE_NAME = "bt-coach-v1";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json"
+const CACHE_NAME="bt-coach-v2";
+const ASSETS=[
+"./","./index.html","./style.css","./app.js","./manifest.json",
+"./apple-touch-icon.png",
+"./icons/icon-192.png","./icons/icon-512.png"
 ];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install",e=>{
+ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate",e=>{
+ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",e=>{
+ if(e.request.method!=="GET") return;
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+   const copy=resp.clone();
+   caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));
+   return resp;
+ }).catch(()=>caches.match("./index.html"))));
 });
